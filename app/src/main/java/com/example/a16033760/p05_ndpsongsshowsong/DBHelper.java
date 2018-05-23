0f987cfdb07a9ca7c339f0a6dc5059409fd71d0c
16033760 <16033760@myrp.edu.sg>
2018-05-23 21:53:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16033760/p05_ndpsongsshowsong/DBHelper.java
@@ -0,0 +1,49 @@
+package com.example.a16033760.p05_ndpsongsshowsong;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "NDPSongs.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NOTE = "song";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_SINGER = "singer";
+    private static final String COLUMN_YEAR = "year";
+    private static final String COLUMN_STARS = "stars";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNoteTableSql = "CREATE TABLE " + TABLE_NOTE + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT," + COLUMN_SINGER + " TEXT," + COLUMN_YEAR + " INTEGER," + COLUMN_STARS + " INTEGER ) ";
+        db.execSQL(createNoteTableSql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+    }
+
+    public long insertSong(String title, String singer, int year, int stars) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITLE, title);
+        values.put(COLUMN_SINGER, singer);
+        values.put(COLUMN_YEAR, year);
+        values.put(COLUMN_STARS, stars);
+        long result = db.insert(TABLE_NOTE, null, values);
+        db.close();
+        Log.d("SQL Insert ",""+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+}
